4a7e11a427066b99b51f2ebf417b06d96746d3df
amircisco <amirjahani69@gmail.com>
2021-09-25 07:41:02 -0400

    update gitignore

--- a.py
+++
@@ -1,5 +0,0 @@
-import jdatetime
-import datetime
-
-#print(type(jdatetime.datetime(year=1400,month=6,day=31).togregorian().date()))
-print(jdatetime.datetime(year=1400, month=6, day=1).togregorian().date())
